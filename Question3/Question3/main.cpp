aa84b99534f8518706c622bfc5659a6da63fba46
Akshita Singh <ams10329@psu.edu>
2024-04-10 00:12:56 -0400

    Question 3

---
+++ Question3/Question3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Question3
+//
+//  Created by Akshita Singh on 4/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
